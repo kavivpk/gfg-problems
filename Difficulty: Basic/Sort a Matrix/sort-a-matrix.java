7e1b0892718cb48b1fb49e5bf4ee38d1d4176657
kaviyarasu V <kaviyarasu4980@gmail.com>
2026-09-09 22:21:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort a Matrix/sort-a-matrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    int[][] sortedMatrix(int mat[][]) {
+        // code here
+        int n=mat.length;
+        int arr[]=new int[n*n];
+        int k=0;
+        int m=mat[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[k]=mat[i][j];
+                k++;
+            }
+        }
+        
+        Arrays.sort(arr);
+        k=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j]=arr[k];
+                k++;
+            }
+        }
+        
+        return mat;
+    }
+}
